eda7f4e9e6019cb76be0f084caf2d7c91d4b97e5
HadasaPines <Hand3D26@mbjcomp.org.il>
2025-05-27 11:44:08 +0300

    add gitignore && techer service dal

--- BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
+++ BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+589f78f5e0344a359c16041481e284f7c5356c55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+add0bab1a6419fa38954bcffd19b6d735101e030")]
 [assembly: System.Reflection.AssemblyProductAttribute("BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
