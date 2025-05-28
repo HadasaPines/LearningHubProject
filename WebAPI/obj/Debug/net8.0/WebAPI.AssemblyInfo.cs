88618edf727d49ec649a3512ad86e42f863f7afc
Dvora-Abrahams <staa2000@gmail.com>
2025-05-29 00:15:11 +0300

    change the connection string

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9be94738ba5ec76ea27b3def8e1483bd9c8ef0d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+750d74a936b0183cf30671fbe40dd99f068eba54")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
