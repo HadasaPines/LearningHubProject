e85c5447548ee36535084d9cdecbce4903dbbdb2
HadasaPines <D0548548024@gmail.com>
2025-06-15 18:14:03 +0300

    fix user & teacher in db

--- BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
+++ BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0a97526e96355a12ceae31ab78aad4dc1e635e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+138932137ee930397738a002c9a7067dee6a24b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
