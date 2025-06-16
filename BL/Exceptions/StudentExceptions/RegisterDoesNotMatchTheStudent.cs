01cfd93e9761eb7350159a60b8790240b8ca31e8
HadasaPines <D0548548024@gmail.com>
2025-06-16 14:30:02 +0300

    Continue build student managment

---
+++ BL/Exceptions/StudentExceptions/RegisterDoesNotMatchTheStudent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Exceptions.StudentExceptions
+{
+    internal class RegisterDoesNotMatchTheStudent: Exception
+    {
+        public RegisterDoesNotMatchTheStudent() : base($"The student's ID does not match the student's ID in this registration.")
+        {
+
+        }
+        public int? StatusCode { get; } = 440;
+    }
+}
