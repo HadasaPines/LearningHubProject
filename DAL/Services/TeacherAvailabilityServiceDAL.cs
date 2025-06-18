5fd8c5b812568be3c996b6cbe6257758984527d4
OritCohenZada <hand3d19@mbjcomp.orj.il>
2025-06-18 21:10:55 +0300

    feat: finish teacherAvailability managment

--- DAL/Services/TeacherAvailabilityServiceDAL.cs
+++ DAL/Services/TeacherAvailabilityServiceDAL.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DAL.Contexts;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,64 @@ using System.Threading.Tasks;
 
 namespace DAL.Services
 {
-    internal class TeacherAvailabilityServiceDAL
+    public class TeacherAvailabilityServiceDAL
     {
-    }
+        private readonly LearningHubDbContext dbContext;
+        public TeacherAvailabilityServiceDAL(LearningHubDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+
+        }
+        public async Task AddTeacherAvailability(TeacherAvailability teacherAvailability)
+        {
+            await dbContext.TeacherAvailabilities.AddAsync(teacherAvailability);
+            await dbContext.SaveChangesAsync();
+        }
+        public async Task<TeacherAvailability> UpdateTeacherAvailability(TeacherAvailability teacherAvailability)
+        {
+
+
+            dbContext.TeacherAvailabilities.Update(teacherAvailability);
+            await dbContext.SaveChangesAsync();
+            return teacherAvailability;
+
+        }
+
+        public async Task DeleteTeacherAvailability(TeacherAvailability teacherAvailability)
+        {
+
+            dbContext.TeacherAvailabilities.Remove(teacherAvailability);
+            await dbContext.SaveChangesAsync();
+        }
+
+
+        public async Task<List<TeacherAvailability>> GetAllTeacherAvailabilities()
+        {
+            return await dbContext.TeacherAvailabilities.ToListAsync();
+        }
+
+        public async Task<List<TeacherAvailability>> GetTeacherAvailabilitiesByWeekDay(int weekDay)
+        {
+            return await dbContext.TeacherAvailabilities
+                .Where(ta => ta.WeekDay == weekDay)
+                .ToListAsync();
+        }
+        public async Task<List<TeacherAvailability>> GetTeacherAvailabilitiesByTimeRange(TimeOnly startTime, TimeOnly endTime)
+        {
+            return await dbContext.TeacherAvailabilities
+                .Where(ta => ta.StartTime >= startTime && ta.EndTime <= endTime)
+                .ToListAsync();
+
+
+
+        }
+        public async Task<TeacherAvailability> GetTeacherAvailabilitiesById(int id)
+        {
+            return await dbContext.TeacherAvailabilities
+                .FirstOrDefaultAsync(ta => ta.AvailabilityId == id);
+                
+
+
+
+        }
 }
