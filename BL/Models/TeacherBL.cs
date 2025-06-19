40e9e2d2b56375d81587f67b0d5e0a2e1ca26e49
HadasaPines <D0548548024@gmail.com>
2025-06-20 02:08:38 +0300

    add student & subject & registration validation

--- BL/Models/TeacherBL.cs
+++ BL/Models/TeacherBL.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BL.ValidationAttributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +10,17 @@ namespace BL.Models
 {
     public class TeacherBL
     {
-        public int TeacherId { get; set; }
+        [Required(ErrorMessage = "Teacher ID is required")]
+        [IsraeliId(ErrorMessage = "Invalid Israeli ID number")]
+        public int TeacherId { get; set; } 
 
         public string? Bio { get; set; }
-
+        [RegularExpression("^(M|F)$", ErrorMessage = "Gender must be M or F")]
+        [Required(ErrorMessage = "Gender is required")]
         public string Gender { get; set; } = null!;
 
+        [DataType(DataType.Date)]
+        [BirthDateInPast(ErrorMessage = "BirthDate must be in the past")]
         public DateOnly? BirthDate { get; set; }
 
     }
--- BL/Models/UserBL.cs
+++ BL/Models/UserBL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BL.ValidationAttributes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -11,7 +12,7 @@ namespace BL.Models
     public class UserBL
     {
 
-
+        [IsraeliId]
         public int UserId { get; set; }
 
         [Required(ErrorMessage = "First name is required")]
