9c3863ae957b3f5a08b2702dd3e5d26b290f6948
OritCohenZada <hand3d19@mbjcomp.orj.il>
2025-07-09 17:23:12 +0300

    feat: add service DAL,service BL and controller to payment

---
+++ backend/BL/Api/IPaymentServiceBL.cs
@@ -0,0 +1,13 @@
+﻿using BL.Models;
+
+namespace BL.Api
+{
+    public interface IPaymentServiceBL
+    {
+        Task AddPayment(PaymentBL paymentBL);
+        Task DeletePayment(int paymentId);
+        Task<List<PaymentBL>> GetAllPayments();
+        Task<PaymentBL> GetPaymentById(int paymentId);
+        Task<List<PaymentBL>> GetPaymentsByStudentId(int studentId);
+    }
+}
---
+++ backend/BL/Services/PaymentServiceBL.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using BL.Exceptions.PaymentExceptions;
+using BL.Models;
+using DAL.Api;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Services
+{
+    public class PaymentServiceBL : IPaymentServiceBL
+    {
+        IPaymentServiceDAL _paymentServiceDAL;
+        IMapper _mapper;
+        public PaymentServiceBL(IPaymentServiceDAL paymentServiceDAL, IMapper mapper)
+        {
+            _paymentServiceDAL = paymentServiceDAL;
+            _mapper = mapper;
+        }
+        public async Task AddPayment(PaymentBL paymentBL)
+        {
+            if (paymentBL == null)
+                throw new ArgumentNullException(nameof(paymentBL), "Payment cannot be null");
+            var payment = _mapper.Map<Payment>(paymentBL);
+            await _paymentServiceDAL.AddPayment(payment);
+        }
+        public async Task<List<PaymentBL>> GetAllPayments()
+        {
+            var payments = await _paymentServiceDAL.GetAllPayments();
+            if (payments == null || !payments.Any())
+                throw new PaymentNotFoundException("No payments found");
+            return _mapper.Map<List<PaymentBL>>(payments);
+        }
+        public async Task<PaymentBL> GetPaymentById(int paymentId)
+        {
+            var payment = await _paymentServiceDAL.GetPaymentById(paymentId);
+            if (payment == null)
+                throw new PaymentNotFoundException($"Payment with ID '{paymentId}' not found");
+            return _mapper.Map<PaymentBL>(payment);
+        }
+
+        public async Task DeletePayment(int paymentId)
+        {
+            var payment = await _paymentServiceDAL.GetPaymentById(paymentId);
+            if (payment == null)
+                throw new PaymentNotFoundException($"Payment with ID '{paymentId}' not found");
+            await _paymentServiceDAL.DeletePayment(paymentId);
+        }
+        public async Task<List<PaymentBL>> GetPaymentsByStudentId(int studentId)
+        {
+            var payments = await _paymentServiceDAL.GetPaymentsByStudentId(studentId);
+            if (payments == null || !payments.Any())
+                throw new PaymentNotFoundException($"No payments found for student with ID '{studentId}'");
+            return _mapper.Map<List<PaymentBL>>(payments);
+
+        }
+
+    }
+}
