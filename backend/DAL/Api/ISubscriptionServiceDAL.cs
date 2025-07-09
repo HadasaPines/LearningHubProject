1fd50d15aba933ae7fc7db6e6d532dfb5542149f
OritCohenZada <hand3d19@mbjcomp.orj.il>
2025-07-09 15:05:28 +0300

    feat: add service DAL, service BL and controller to subscriptions

---
+++ backend/DAL/Api/ISubscriptionServiceDAL.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Models;
+
+namespace DAL.Api
+{
+    public interface ISubscriptionServiceDAL
+    {
+        Task AddSubscription(Subscription subscription);
+        Task DeleteSubscription(int id);
+        Task<List<Subscription>> GetAllSubscriptions();
+        Task<Subscription> GetSubscriptionById(int id);
+        Task UpdateSubscription(Subscription subscription);
+    }
+}
---
+++ backend/DAL/Services/SubscriptionServiceDAL.cs
@@ -0,0 +1,58 @@
+﻿using DAL.Contexts;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Services
+{
+    public class SubscriptionServiceDAL : ISubscriptionServiceDAL
+    {
+        private readonly LearningHubDbContext _dbContext;
+        public SubscriptionServiceDAL(LearningHubDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddSubscription(Subscription subscription)
+        {
+            await _dbContext.Subscriptions.AddAsync(subscription);
+            await _dbContext.SaveChangesAsync();
+        }
+        public async Task<List<Subscription>> GetAllSubscriptions()
+        {
+            return await _dbContext.Subscriptions.ToListAsync();
+        }
+        public async Task<Subscription> GetSubscriptionById(int id)
+        {
+            return await _dbContext.Subscriptions
+                .FirstOrDefaultAsync(s => s.SubscriptionId == id);
+        }
+
+        public async Task UpdateSubscription(Subscription subscription)
+        {
+            var existingSubscription = await _dbContext.Subscriptions
+                .FirstOrDefaultAsync(s => s.SubscriptionId == subscription.SubscriptionId);
+            if (existingSubscription != null)
+            {
+                _dbContext.Entry(existingSubscription).CurrentValues.SetValues(subscription);
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+        public async Task DeleteSubscription(int id)
+        {
+            var subscription = await _dbContext.Subscriptions
+                .FirstOrDefaultAsync(s => s.SubscriptionId == id);
+            if (subscription != null)
+            {
+                _dbContext.Subscriptions.Remove(subscription);
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+
+
+    }
+}
