1f29aacd617c80388113177092d8fdc213fece61
OritCohenZada <hand3d19@mbjcomp.orj.il>
2025-07-09 16:38:07 +0300

    feat: add service DAL,service BL and controller to studentSubscription

---
+++ backend/DAL/Api/IStudentSubscriptionServiceDAL.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Models;
+
+namespace DAL.Api
+{
+    public interface IStudentSubscriptionServiceDAL
+    {
+        Task AddStudentSubscription(StudentSubscription studentSubscription);
+        Task DeleteStudentSubscription(int id);
+        Task<List<StudentSubscription>> GetAllStudentSubscriptions();
+        Task<List<StudentSubscription>> GetStudentSubscriptionById(int id);
+        Task UpdateLessonsUsed(StudentSubscription studentSubscription);
+    }
+}
---
+++ backend/DAL/Services/StudentSubscriptionServiceDAL.cs
@@ -0,0 +1,54 @@
+﻿using DAL.Contexts;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Services
+{
+    public class StudentSubscriptionServiceDAL : IStudentSubscriptionServiceDAL
+    {
+        private readonly LearningHubDbContext _context;
+        public StudentSubscriptionServiceDAL(LearningHubDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+        public async Task<List<StudentSubscription>> GetAllStudentSubscriptions()
+        {
+            return await _context.StudentSubscriptions.ToListAsync();
+        }
+        public async Task<List<StudentSubscription>> GetStudentSubscriptionById(int id)
+        {
+            return await _context.StudentSubscriptions
+                .Where(ss => ss.StudentSubscriptionId == id).ToListAsync();
+        }
+        public async Task AddStudentSubscription(StudentSubscription studentSubscription)
+        {
+            await _context.StudentSubscriptions.AddAsync(studentSubscription);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteStudentSubscription(int id)
+        {
+            var studentSubscription = await _context.StudentSubscriptions
+                .FirstOrDefaultAsync(ss => ss.StudentSubscriptionId == id);
+            _context.StudentSubscriptions.Remove(studentSubscription);
+            await _context.SaveChangesAsync();
+
+        }
+
+        public async Task UpdateLessonsUsed(StudentSubscription studentSubscription)
+        {
+            var existingSubscription = await _context.StudentSubscriptions
+                .FirstOrDefaultAsync(ss => ss.StudentSubscriptionId == studentSubscription.StudentSubscriptionId);
+            _context.Entry(existingSubscription).CurrentValues.SetValues(studentSubscription);
+            await _context.SaveChangesAsync();
+        }
+
+
+    }
+}
+
